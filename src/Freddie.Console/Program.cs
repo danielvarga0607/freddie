b8e1e38a45ea531d2e48360b1bc6242a254f9edf
Daniel Varga <varga.daniel@nexius.hu>
2023-03-09 10:49:13 +0100

    create console app for testing Task.WhenAll method and add to solution file

---
+++ src/Freddie.Console/Program.cs
@@ -0,0 +1,15 @@
+﻿await Task.WhenAll(Task1(), Task2())
+    .ContinueWith(task => Console.WriteLine(task.Exception?.Message), TaskContinuationOptions.OnlyOnFaulted)
+    .ConfigureAwait(false);
+
+Task Task1()
+{
+    Thread.Sleep(2000);
+    throw new Exception(nameof(Task1) + " something went wrong!");
+}
+
+Task Task2()
+{
+    Thread.Sleep(1000);
+    throw new Exception(nameof(Task2) + " something other when wrong!");
+}
